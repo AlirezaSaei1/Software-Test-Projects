b10060c93b894c0f555ac92f1ea054ebb8534b8d
Alireza Saei <saeialireza359@gmail.com>
2023-11-19 22:53:37 +0330

    Wrote a test for creating valid trips

--- Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
+++ Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.example.*;
 
+import java.time.Instant;
 import java.time.ZoneId;
 import java.util.List;
 
 public class Tests {
+    // Scenario 1: City and Train Management
     @Test
     public void addNewCity(){
         ZoneId zoneId = ZoneId.systemDefault();
@@ -60,5 +62,32 @@ public class Tests {
             }
         }
         assertTrue(trainExists);
+    }
+
+    // Scenario 2: Trip Management
+    @Test
+    public void createNewValidTrip() throws TripException {
+        ZoneId zoneId = ZoneId.systemDefault();
+        TicketReservationSystem trs = new TicketReservationSystemImpl(zoneId);
+
+        City origin1 = new CityImpl("City A");
+        City destination1 = new CityImpl("City B");
+        Train train1 = new TrainImpl("Express Train", 200);
+        Instant departureTime1 = Instant.parse("2023-11-25T10:00:00Z");
+        Instant arrivalTime1 = Instant.parse("2023-11-28T14:00:00Z");
+
+        Trip trip1 = trs.createTrip(origin1, destination1, train1, departureTime1, arrivalTime1);;
+
+
+        City origin2 = new CityImpl("City C");
+        City destination2 = new CityImpl("City D");
+        Train train2 = new TrainImpl("Local Train", 100);
+        Instant departureTime2 = Instant.parse("2023-12-02T08:00:00Z");
+        Instant arrivalTime2 = Instant.parse("2023-12-02T10:00:00Z");
+
+        Trip trip2 = trs.createTrip(origin2, destination2, train2, departureTime2, arrivalTime2);
+
+        List<Trip> trips = trs.getAllTrips();
+        assertEquals(trips.toArray().length, 2);
     }
 }

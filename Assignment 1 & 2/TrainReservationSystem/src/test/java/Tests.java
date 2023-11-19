aba7af4b50b46bebfea9e20d99b6fdd1c809c07a
Alireza Saei <saeialireza359@gmail.com>
2023-11-19 21:31:17 +0330

    Wrote a test for adding a city

--- Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
+++ Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
@@ -1,13 +1,41 @@
+import org.example.impl.CityImpl;
 import org.example.impl.TicketImpl;
+import org.example.impl.TicketReservationSystemImpl;
 import org.example.impl.TripImpl;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.example.*;
+
+import java.time.ZoneId;
+import java.util.List;
 
 public class Tests {
     @Test
-    public void firstTest(){
+    public void addNewCity(){
+        ZoneId zoneId = ZoneId.systemDefault();
+        TicketReservationSystem trs = new TicketReservationSystemImpl(zoneId);
 
+        // Add a city to the system before executing the test
+        City existingCity1 = new CityImpl("Los Angeles");
+        trs.addCity(existingCity1);
+        City existingCity2 = new CityImpl("Washington");
+        trs.addCity(existingCity2);
+        City existingCity3 = new CityImpl("Texas");
+        trs.addCity(existingCity3);
+
+        City city = new CityImpl("California");
+        trs.addCity(city);
+        List<City> cityList = trs.getCities();
+
+        boolean cityExists = false;
+        for (City c : cityList) {
+            if (c.getName().equals(city.getName())) {
+                cityExists = true;
+                break;
+            }
+        }
+        assertTrue(cityExists);
     }
 
 }

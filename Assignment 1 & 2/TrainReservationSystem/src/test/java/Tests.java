f06eea2defe94b254beca58ae0a04b52ea7a30fc
Alireza Saei <saeialireza359@gmail.com>
2023-11-20 14:43:52 +0330

    Wrote a test for booking an invalid ticket

--- Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
+++ Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
@@ -152,4 +152,25 @@ public class Tests {
         boolean ticketIsCancelled = trip.getBookedTickets().isEmpty();
         assertTrue(ticketIsCancelled);
     }
+
+    @Test
+    public void bookAnInvalidTicket() throws TripException, ReservationException{
+        ZoneId zoneId = ZoneId.systemDefault();
+        TicketReservationSystem trs = new TicketReservationSystemImpl(zoneId);
+
+        City origin = new CityImpl("City A");
+        City destination = new CityImpl("City B");
+        Train train = new TrainImpl("Express Train", 3);
+        Instant departureTime = Instant.parse("2023-11-25T10:00:00Z");
+        Instant arrivalTime = Instant.parse("2023-11-28T14:00:00Z");
+
+        Trip trip = trs.createTrip(origin, destination, train, departureTime, arrivalTime);
+        trip.bookTicket("Alireza Saei");
+        trip.bookTicket("David Goggins");
+        trip.bookTicket("Donald Trump");
+
+        assertThrows(ReservationException.class, () -> {
+            trip.bookTicket("Andrew Tate");
+        });
+    }
 }

6f0ed7bfe9feca35e197ec97930bd8515c98707a
Alireza Saei <saeialireza359@gmail.com>
2023-11-20 19:37:14 +0330

    Added a test for checking departure time is before arrival time after adding delay

--- Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
+++ Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
@@ -151,7 +151,11 @@ public class Tests {
         trip.cancelTicket(tickets.get(0));
 
         boolean ticketIsCancelled = trip.getBookedTickets().isEmpty();
+
+        List<Ticket> canceled = trs.getAllCancelledTickets();
+
         assertTrue(ticketIsCancelled);
+        assertFalse(canceled.isEmpty());
     }
 
     @Test
@@ -223,5 +227,28 @@ public class Tests {
         assertEquals(trip.getPlannedArrivalTime(), Instant.parse("2023-11-28T14:00:00Z"));
     }
 
-    
+    @Test
+    public void addDepartureDelayToATripMoreThanDuration() throws TripException{
+        ZoneId zoneId = ZoneId.systemDefault();
+        TicketReservationSystem trs = new TicketReservationSystemImpl(zoneId);
+
+        City origin = new CityImpl("City A");
+        City destination = new CityImpl("City B");
+        Train train = new TrainImpl("Express Train", 10);
+        Instant departureTime = Instant.parse("2023-11-25T10:00:00Z");
+        Instant arrivalTime = Instant.parse("2023-11-27T10:00:00Z");
+
+        Trip trip = trs.createTrip(origin, destination, train, departureTime, arrivalTime);
+        Duration dur = Duration.ofDays(4);
+        trip.addDepartureDelay(dur);
+
+        assertTrue(trs.getAllTrips().get(0).isDelayed());
+        assertEquals(trip.getDepartureDelay(), dur);
+        assertEquals(trip.findRealDepartureTime(), Instant.parse("2023-11-29T10:00:00Z"));
+        assertEquals(trip.getPlannedDepartureTime(), Instant.parse("2023-11-25T10:00:00Z"));
+        // Now departure time is after arrival time that is not true
+        assertTrue(trip.findRealArrivalTime().isAfter(trip.findRealDepartureTime()));
+    }
+
+    // Scenario 5: Exchange Management
 }

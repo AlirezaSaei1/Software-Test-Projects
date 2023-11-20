43c98d2ace559c8a6c47daa9bcc76267c7007793
Alireza Saei <saeialireza359@gmail.com>
2023-11-20 14:29:19 +0330

    Added a test for canceling a ticket

--- Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
+++ Assignment 1 & 2/TrainReservationSystem/src/test/java/Tests.java
@@ -131,4 +131,25 @@ public class Tests {
 
         assertEquals(tickets, actual_tickets);
     }
+
+    @Test
+    public void cancelATicket() throws TripException, ReservationException{
+        ZoneId zoneId = ZoneId.systemDefault();
+        TicketReservationSystem trs = new TicketReservationSystemImpl(zoneId);
+
+        City origin = new CityImpl("City A");
+        City destination = new CityImpl("City B");
+        Train train = new TrainImpl("Express Train", 200);
+        Instant departureTime = Instant.parse("2023-11-25T10:00:00Z");
+        Instant arrivalTime = Instant.parse("2023-11-28T14:00:00Z");
+
+        Trip trip = trs.createTrip(origin, destination, train, departureTime, arrivalTime);
+        trip.bookTicket("Alireza");
+
+        List<Ticket> tickets = trs.getAllBookedTickets();
+        trip.cancelTicket(tickets.get(0));
+
+        boolean ticketIsCancelled = trip.getBookedTickets().isEmpty();
+        assertTrue(ticketIsCancelled);
+    }
 }

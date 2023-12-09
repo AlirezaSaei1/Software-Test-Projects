674bf8a9847f0b61284e9e755d95cbd8ff4977a6
Alireza Saei <saeialireza359@gmail.com>
2023-12-09 15:51:32 +0330

    Added a code to circumvent throw of an exception when removing

--- ArrayListRemoveException/ArrayListRemoveCircumvent/src/Main.java
+++ ArrayListRemoveException/ArrayListRemoveCircumvent/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,9 +11,23 @@ public class Main {
             list.add(i);
         }
 
-        // This will throw ConcurrentModificationException
-        for (var num: list){
-            list.remove(0);
+        // This will now throw ConcurrentModificationException
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            int element = iterator.next();
+            if (element == 1) {
+                iterator.remove();
+            }
+        }
+        System.out.println("Modified List: " + list);
+
+        try {
+            // This will throw ConcurrentModificationException
+            for (var num : list) {
+                list.remove(0);
+            }
+        }catch (ConcurrentModificationException e){
+            System.out.println("Thrown -> " + e.toString());
         }
     }
 }
